9ceb81c4ccc8133f60261c5cd51969c4a88da5e3
Joseph LoPresti <josephlopresti.student@careerdevs.com>
2019-04-11 23:47:56 +0000

    finished version 1

--- react-redux-2/src/App.js
+++ react-redux-2/src/App.js
@@ -2,13 +2,20 @@ import React, { Component } from 'react';
 import Ninjas from './Ninjas';
 
 class App extends Component {
+  state = {
+    ninjas: [
+      { name: "Joe", age: "25", belt: "Purple", id: 1 }, 
+      { name: "Mike", age: "42", belt: "Brown", id: 2 }, 
+      { name: "Mitch", age: "29", belt: "Black", id: 3 }, 
+      { name: "Sara", age: "30", belt: "Orange", id: 4 }
+    ]
+  }
   render() {
     return (
       <div className="App">
         <h1>My first react app!</h1>
         <p>Welcome!</p>
-        <Ninjas name="Joe" age="24" belt="purple" />
-        <Ninjas name="Mike" age="50" belt="brown" />
+        <Ninjas ninjas={this.state.ninjas} />
       </div>
     );
   }
